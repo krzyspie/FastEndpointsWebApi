98576744a584ce8d977a5d2234a0e1e98819d36b
krzpie <krzys_7@op.pl>
2025-04-22 21:18:24 +0200

    update add car request validator

--- FastEndpointsWebApi/FastEndpointsWebApi/Cars/Add/AddCarValidator.cs
+++ FastEndpointsWebApi/FastEndpointsWebApi/Cars/Add/AddCarValidator.cs
@@ -1,4 +1,5 @@
 using FastEndpoints;
+using FastEndpointsWebApi.Cars.Entities;
 using FluentValidation;
 
 namespace FastEndpointsWebApi.Cars.Add;
@@ -18,5 +19,17 @@ public class AddCarValidator : Validator<AddCarRequest>
         RuleFor(x => x.Year)
             .InclusiveBetween(1886, DateTime.Now.Year)
             .WithMessage($"Year must be between 1886 and {DateTime.Now.Year}.");
+                
+        RuleFor(x => x.EngineType)
+            .NotEmpty()
+            .WithMessage("EngineType is required.")
+            .Must(x => Enum.TryParse(typeof(EngineType), x, out _))
+            .WithMessage("Invalid EngineType value.");
+        
+        RuleFor(x => x.Transmission)
+            .NotEmpty()
+            .WithMessage("Transmission is required.")
+            .Must(x => Enum.TryParse(typeof(Transmission), x, out _))
+            .WithMessage("Invalid Transmission value.");
     }
 }
