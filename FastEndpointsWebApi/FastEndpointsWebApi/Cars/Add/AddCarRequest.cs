0730fa19819515ae224cb82133f27cc63c6880d4
krzpie <krzys_7@op.pl>
2025-04-22 21:16:26 +0200

    extend add car request

--- FastEndpointsWebApi/FastEndpointsWebApi/Cars/Add/AddCarRequest.cs
+++ FastEndpointsWebApi/FastEndpointsWebApi/Cars/Add/AddCarRequest.cs
@@ -5,4 +5,8 @@ public class AddCarRequest
     public string Make { get; set; } = string.Empty;
     public string Model { get; set; } = string.Empty;
     public int Year { get; set; }
+    public string Color { get; set; } = string.Empty;
+    public string EngineType { get; set; } = string.Empty;
+    public string Transmission { get; set; } = string.Empty;
+    public int Horsepower { get; set; }
 }
