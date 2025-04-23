3ebba2f1c13fcd9388de5d28970acfaac5c7c897
krzpie <krzys_7@op.pl>
2025-04-23 22:05:58 +0200

    car make enum

---
+++ FastEndpointsWebApi/FastEndpointsWebApi/Cars/Entities/CarMake.cs
@@ -0,0 +1,28 @@
+namespace FastEndpointsWebApi.Cars.Entities;
+
+public enum CarMake
+{
+    Toyota = 1,
+    Ford = 2,
+    Honda = 3,
+    Chevrolet = 4,
+    Nissan = 5,
+    BMW = 6,
+    Mercedes = 7,
+    Audi = 8,
+    Volkswagen = 9,
+    Hyundai = 10,
+    Kia = 11,
+    Subaru = 12,
+    Mazda = 13,
+    Lexus = 14,
+    Infiniti = 15,
+    Acura = 16,
+    Volvo = 17,
+    LandRover = 18,
+    Jaguar = 19,
+    Tesla = 20,
+    Porsche = 21,
+    Fiat = 22,
+    Jeep = 23,
+}
